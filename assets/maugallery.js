e178b86111b7d277aa6f4309d9f48836912bc8d2
Bulmxnd <lfilipe.mottaa@gmail.com>
2025-07-16 04:29:41 +0200

    [+] Debugged initial code

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -162,7 +162,7 @@
       let activeImage = null;
       $("img.gallery-item").each(function() {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
+          activeImage = $(this)
         }
       });
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
